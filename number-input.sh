0a743ef3e116d2f7200b1186e75adf5d87fa1111
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-13 10:13:33 +0200

    Convert gbs to mbs

--- number-input.sh
+++ number-input.sh
@@ -9,19 +9,22 @@ while :; do
     read -r INPUT
 
     # Convert string to uppercase
-    INPUT=$(echo "$INPUT" | tr '[:lower:]' '[:upper:]')
+    INPUT=$(tr '[:lower:]' '[:upper:]' <<< "$INPUT")
 
     if [[ "$INPUT" =~ $PATTERN ]]; then
-        # TODO: Convert swap from G to M if needed
-
+        # Get size unit
+        SIZE_UNIT=$(tr -dc "[:upper:]" <<< "$INPUT")
 
         # Get numbers only and clear trailing zeroes
         INPUT=$(echo "$INPUT" | tr -dc '0-9' | sed -E 's/^0+//')
 
+        # Convert GBs to MBs
+        if [[ "$SIZE_UNIT" = "G" ]]; then INPUT=$((INPUT * 1024)); fi
+
         if ((INPUT >= LOWER_LIMIT)); then
             break
         else
-            echo "Value is too low. Try one that is equal to or bigger than ${LIMIT}M"
+            echo "Value is too low. Try one that is equal to or bigger than ${LOWER_LIMIT}M"
         fi
     else
         printf "Wrong value! Use this format: $PATTERN\n"
